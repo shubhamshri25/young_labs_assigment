a7f01f775a28e72991bd86e4f15acb47c22ab423
shubham <shubhshri25gmail.com>
2025-03-06 19:46:20 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,13 @@ const cors = require("cors");
 
 const greetRoute = require("./routes/greet.route");
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*", 
+    methods: ["GET", "POST"], 
+    allowedHeaders: ["Content-Type", "Authorization"], 
+  })
+);
 
 app.use("/api", greetRoute);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
