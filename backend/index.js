5eeee70a95f90f543dd13d870a91d90e5598122c
shubham <shubhshri25gmail.com>
2025-03-06 19:52:18 +0530

    fixing cors

--- backend/index.js
+++ backend/index.js
@@ -5,13 +5,8 @@ const cors = require("cors");
 
 const greetRoute = require("./routes/greet.route");
 
-app.use(
-  cors({
-    origin: "*", 
-    methods: ["GET", "POST"], 
-    allowedHeaders: ["Content-Type", "Authorization"], 
-  })
-);
+app.use(cors());
 
 app.use("/api", greetRoute);
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
