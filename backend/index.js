f6ba1ed9e4c5f4d53363d70cc4b433aa8cd50404
shubham <shubhshri25gmail.com>
2025-03-06 19:54:48 +0530

    cors global

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,9 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const cors = require("cors");
+app.use(cors());
 
 const greetRoute = require("./routes/greet.route");
-
-app.use(cors());
 
 app.use("/api", greetRoute);
 
